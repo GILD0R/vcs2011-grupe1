f2eddda17f9a89c40e93e2c05c10460b67ac7e8a
ReditaJ <redita.j@gmail.com>
2020-11-20 01:11:42 +0200

    Finland pradinis

---
+++ finland.php
@@ -0,0 +1,49 @@
+
+<?php include "header.php" ?>
+
+
+<body>
+    <div class="container">
+    <h1 class="white darken-4 center-align">Finland</h1>
+    
+<p>Finland, country located in northern Europe. Finland is one of the world’s most northern and geographically remote countries and is subject to a severe climate. Nearly two-thirds of Finland is blanketed by thick woodlands, making it the most densely forested country in Europe. Finland also forms a symbolic northern border between western and eastern Europe: dense wilderness and Russia to the east, the Gulf of Bothnia and Sweden to the west.</p>
+
+<div class = "row" >
+    <div class="col s4 white center-align" >
+        <img class="responsive-img" src="images/finland1.jpeg" alt="">
+        <a class="waves-effect waves-teal btn-flat">NAME</a>       
+         <p class="z-depth-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi dolorum fugit non corrupti ea facere totam possimus debitis, odio error placeat, porro delectus vitae, aperiam quam. Repudiandae eos mollitia odio!</p>
+    </div>
+    <div class="col s4 white center-align">
+        <img class="responsive-img" src="images/finland2.jpg" alt="">
+        <a class="waves-effect waves-teal btn-flat">NAME</a>          
+        <p class="z-depth-4"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi dolorum fugit non corrupti ea facere totam possimus debitis, odio error placeat, porro delectus vitae, aperiam quam. Repudiandae eos mollitia odio!</p>
+    </div>
+    <div class="col s4 white center-align">
+        <img class="responsive-img" src="images/finland3.jpg" alt="">
+        <a class="waves-effect waves-teal btn-flat">NAME</a>  
+        <p class="z-depth-4" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi dolorum fugit non corrupti ea facere totam possimus debitis, odio error placeat, porro delectus vitae, aperiam quam. Repudiandae eos mollitia odio!</p>
+    </div>
+
+    <div class="row">
+    <div class="col s12 m7 center-align">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/finland4.jpg">
+          <span class="card-title">Polar lights</span>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+      </div>
+    </body>
+
+
+<?php include "footer.php" ?>
