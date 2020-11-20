716e49d3272b771ef192587962da2880c06a6db3
GILD0R <sigmarite@gmail.com>
2020-11-20 20:22:22 +0200

    initial header

--- header.php
+++ header.php
@@ -1,30 +1,43 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link rel="stylesheet" href="styles/style.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <link rel="stylesheet" href="styles/style.css">
 
 
-    <title>Projekto pavadinimas</title>
+  <title>Scandinavian Embassy</title>
 </head>
 
 <body>
-<nav>
+  <img class="brand-logo" src="images/another-H.jpg" alt="logo">
+  <img class="brand-logo" src="images/newHeader.jpg" alt="logo">
+  <img class="brand-logo" src="images/header-image.jpg" alt="logo">
+  
+  <div class="row teal lighten-4 flag-menu" >
+    <div class="col s4 offset-s8">
+      <a href="norway.html"><img src="images/flag-norway-M.jpg" class="circle responsive-img" alt="">Visit Norway</a>
+      <a href="sweden.html"><img src="images/flag-sweden-M.jpg" class="circle" alt="">Visit Sweden</a>
+      <a href="finland.html"><img src="images/flag-finland-M.jpg" class="circle" alt="">Visit Finland</a>
+      <a href="iceland.html"><img src="images/flag-iceland-M.jpg" class="circle" alt="">Fuck Iceland</a>
+      <a href="denmark.html"><img src="images/flag-denmark-M.jpg" class="circle" alt="">Screw Denmark</a>
+      
+    </div>
+  </div>
+  <nav class="purple">
     <div class="nav-wrapper">
-      <a href="#" class="brand-logo">Logo</a>
+
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
+        <!-- <li><a href="norway.html"><img src="images/flag-norway-M.jpg" class="circle responsive-img" alt=""> Norway</a></li>
+        <li><a href="sweden.html"><img src="images/flag-sweden-M.jpg" class="circle" alt="">Sweden</a></li>
+        <li><a href="finland.html"><img src="images/flag-finland-M.jpg" class="circle" alt="">Finland</a></li> -->
       </ul>
     </div>
   </nav>
 
-    <!-- Čia pasikartojanti visuose puslapiuose viršutinė dalis -->
+  <!-- Čia pasikartojanti visuose puslapiuose viršutinė dalis -->
