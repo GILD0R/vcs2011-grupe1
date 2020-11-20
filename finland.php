33acb8cf6ec3f15c09011ba99df23c18ba3941a2
ReditaJ <redita.j@gmail.com>
2020-11-20 12:38:32 +0200

    Finland korekcija

--- finland.php
+++ finland.php
@@ -4,45 +4,57 @@
 
 <body>
     <div class="container">
-    <h1 class="white darken-4 center-align">Finland</h1>
-    
-<p>Finland, country located in northern Europe. Finland is one of the world’s most northern and geographically remote countries and is subject to a severe climate. Nearly two-thirds of Finland is blanketed by thick woodlands, making it the most densely forested country in Europe. Finland also forms a symbolic northern border between western and eastern Europe: dense wilderness and Russia to the east, the Gulf of Bothnia and Sweden to the west.</p>
-
-<div class = "row" >
-    <div class="col s4 white center-align" >
-        <img class="responsive-img" src="images/finland1.jpeg" alt="">
-        <a class="waves-effect waves-teal btn-flat">NAME</a>       
-         <p class="z-depth-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi dolorum fugit non corrupti ea facere totam possimus debitis, odio error placeat, porro delectus vitae, aperiam quam. Repudiandae eos mollitia odio!</p>
-    </div>
-    <div class="col s4 white center-align">
-        <img class="responsive-img" src="images/finland2.jpg" alt="">
-        <a class="waves-effect waves-teal btn-flat">NAME</a>          
-        <p class="z-depth-4"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi dolorum fugit non corrupti ea facere totam possimus debitis, odio error placeat, porro delectus vitae, aperiam quam. Repudiandae eos mollitia odio!</p>
-    </div>
-    <div class="col s4 white center-align">
-        <img class="responsive-img" src="images/finland3.jpg" alt="">
-        <a class="waves-effect waves-teal btn-flat">NAME</a>  
-        <p class="z-depth-4" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi dolorum fugit non corrupti ea facere totam possimus debitis, odio error placeat, porro delectus vitae, aperiam quam. Repudiandae eos mollitia odio!</p>
-    </div>
-
     <div class="row">
-    <div class="col s12 m7 center-align">
+    <div class="col s12">
       <div class="card">
         <div class="card-image">
-          <img src="images/finland4.jpg">
-          <span class="card-title">Polar lights</span>
+          <img src="images/finland1.jpeg">
+          <span class="card-title">FINLAND</span>
         </div>
         <div class="card-content">
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
+          <p>Finland, country located in northern Europe. Finland is one of the world’s most northern and geographically remote countries and is subject to a severe climate. Nearly two-thirds of Finland is blanketed by thick woodlands, making it the most densely forested country in Europe. Finland also forms a symbolic northern border between western and eastern Europe: dense wilderness and Russia to the east, the Gulf of Bothnia and Sweden to the west.</p>
         </div>
         <div class="card-action">
-          <a href="#">This is a link</a>
+          <a href="#">WELCOME!</a>
         </div>
       </div>
     </div>
   </div>
+
+    <div class = "row" >
+    <div class="col s4 center-align" >
+        <img class="responsive-img" class="z-depth-5" src="images/FinlandFlag.jpg" alt="#">
+        <a class="aves-effect waves-light btn-small blue">LIFE & SOCIETY</a>       
+        <p class="z-depth-4">MAKE A MOVE AND EXPERIENCE LIFE IN FINLAND FOR THREE MONTHS</p>
+    </div>
+    <div class="col s4 center-align">
+        <img class="responsive-img" src="images/finland2.jpg" alt="#">
+        <a class="aves-effect waves-light btn-small blue">ENVIRONMENT</a>          
+        <p class="z-depth-4"> FRESH AIR AND QUIET: APP FINDS BEST HELSINKI ROUTES FOR BIKERS AND HIKERS</p>
+    </div>
+    <div class="col s4 center-align">
+        <img class="responsive-img" src="images/finland3.jpg" alt="#">
+        <a class="aves-effect waves-light btn-small blue">NATURE</a>  
+        <p class="z-depth-4" >FLEETING FAME: FINLAND’S “CENTRE OF POPULATION” EXISTS IN THE MIDDLE OF NOWHERE</p>
+
+    </div>
+
+    <div class="col s12">
+    <div class="card horizontal">
+      <div class="card-image">
+        <img src="images/finland4.jpg">
       </div>
+      <div class="card-stacked">
+        <div class="card-content">
+          <p>At times, cyclists in Helsinki can hardly believe their luck. New cycle lanes both on and off roads, as well as indoor and outdoor racks for bike parking, seem to be materialising every week across the capital area.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">Visit Helsinki</a>
+        </div>
+      </div>
+    </div>
+  </div>   
+ </div>
     </body>
 
 
