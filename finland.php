af2a1782442e027e4adcac49ecf5971361ea6e92
ReditaJ <redita.j@gmail.com>
2020-11-24 19:26:37 +0200

    finland paskutinis

--- finland.php
+++ finland.php
@@ -8,12 +8,12 @@
     <div class="row">
     <div class="col s12">
 
-      <div class="main-image-block">
+      <!-- <div class="main-image-block">
       <div class="main-image-wrapper">
         <div class="main-image"></div>
-        <!-- <img class="responsive-img circle" src="images/finland1.jpeg"> -->
+        <img class="responsive-img circle" src="images/finland1.jpeg">
       </div>
-      </div>
+      </div> -->
 
       
       <!-- <div class="pagrindine-nuotrauka"></div> -->
@@ -21,10 +21,10 @@
     </div>
     <div class="col s12">
       <div class="card">
-        <!-- <div class="card-image">
-          
-          <span class="card-title">FINLAND</span>
-        </div> -->
+        <div class="card-image">
+        <img class="responsive-img circle" src="images/finland1.jpg">
+          <span class="card-title"></span>
+        </div>
         <div class="card-content">
           <p>Finland, country located in northern Europe. Finland is one of the world’s most northern and geographically remote countries and is subject to a severe climate. Nearly two-thirds of Finland is blanketed by thick woodlands, making it the most densely forested country in Europe. Finland also forms a symbolic northern border between western and eastern Europe: dense wilderness and Russia to the east, the Gulf of Bothnia and Sweden to the west.</p>
         </div>
@@ -56,7 +56,7 @@
     <div class="col s12">
     <div class="card horizontal">
       <div class="card-image">
-        <img src="images/finland4.jpg">
+        <img src="images/finland4.jpg" class="circle">
       </div>
       <div class="card-stacked">
         <div class="card-content">
@@ -67,6 +67,23 @@
         </div>
       </div>
     </div>
+
+
+    <!-- <div class="col s12">
+    <div class="card horizontal">
+    <div class="video-container">
+        <iframe width="853" height="480" src="//www.youtube.com/embed/Q8TXgCzxEnw?rel=0" frameborder="0" allowfullscreen></iframe>
+      </div>
+      <div class="card-stacked">
+        <div class="card-content">
+          <p>At times, cyclists in Helsinki can hardly believe their luck. New cycle lanes both on and off roads, as well as indoor and outdoor racks for bike parking, seem to be materialising every week across the capital area.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">Visit Helsinki</a>
+        </div>
+      </div>
+    </div> -->
+
   </div>   
  </div>
     </body>
